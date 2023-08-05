1c12463d29bb16a1857613566a0a322d4d491f68
Artemiy <artem2k17xia@yandex.ru>
2023-08-05 16:26:45 +0500

    обновление порта

--- src/app.ts
+++ src/app.ts
@@ -1,50 +1,50 @@
-import { TasksController } from './tasks/tasks.controller';
-import 'reflect-metadata';
-import { inject, injectable } from 'inversify';
-import express, { Express } from 'express';
-import cors from 'cors';
-import { Server } from 'http';
-import { TYPES } from './types';
-import { ILogger } from './logger/logger.interface';
-import { IExeptionFilter } from './errors/exeption.filter.interface';
+import { TasksController } from "./tasks/tasks.controller";
+import "reflect-metadata";
+import { inject, injectable } from "inversify";
+import express, { Express } from "express";
+import cors from "cors";
+import { Server } from "http";
+import { TYPES } from "./types";
+import { ILogger } from "./logger/logger.interface";
+import { IExeptionFilter } from "./errors/exeption.filter.interface";
 
 @injectable()
 export class App {
-	app: Express;
-	server: Server | undefined;
-	port: number;
+  app: Express;
+  server: Server | undefined;
+  port: number | string;
 
-	constructor(
-		@inject(TYPES.Logger) private logger: ILogger,
-		@inject(TYPES.TasksController) private tasksController: TasksController,
-		@inject(TYPES.ExeptionFilter) private exeptionFilter: IExeptionFilter
-	) {
-		this.app = express();
-		this.port = 8000;
-	}
+  constructor(
+    @inject(TYPES.Logger) private logger: ILogger,
+    @inject(TYPES.TasksController) private tasksController: TasksController,
+    @inject(TYPES.ExeptionFilter) private exeptionFilter: IExeptionFilter
+  ) {
+    this.app = express();
+    this.port = process.env.PORT ?? 8000;
+  }
 
-	useMiddleware = (): void => {
-		this.app.use(express.json());
-		this.app.use(cors());
-	};
+  useMiddleware = (): void => {
+    this.app.use(express.json());
+    this.app.use(cors());
+  };
 
-	useRoutes = (): void => {
-		this.app.use('/api', this.tasksController.router);
-	};
+  useRoutes = (): void => {
+    this.app.use("/api", this.tasksController.router);
+  };
 
-	useExeptionFilters = (): void => {
-		this.app.use(this.exeptionFilter.catch);
-	};
+  useExeptionFilters = (): void => {
+    this.app.use(this.exeptionFilter.catch);
+  };
 
-	public init = (): void => {
-		this.useMiddleware();
-		this.useRoutes();
-		this.useExeptionFilters();
-		this.server = this.app.listen(this.port);
-		this.logger.log(`Сервер запущен на ${this.port} порту`);
-	};
+  public init = (): void => {
+    this.useMiddleware();
+    this.useRoutes();
+    this.useExeptionFilters();
+    this.server = this.app.listen(this.port);
+    this.logger.log(`Сервер запущен на ${this.port} порту`);
+  };
 
-	public close(): void {
-		this.server?.close();
-	}
+  public close(): void {
+    this.server?.close();
+  }
 }
