9ec07732e8f42d5b1530b13720186e395f4dd0c3
Artemiy <artem2k17xia@yandex.ru>
2023-08-17 21:36:22 +0500

    обновление отправки формы

--- src/js/modules/submittingForm.js
+++ src/js/modules/submittingForm.js
@@ -12,38 +12,50 @@ export const submittingForm = () => {
 		loadingMessage.textContent = 'бегу создавать задачу...';
 
 		const formData = new FormData(form);
+		let obj;
+		let url;
 		console.log(formData.get('date'));
+		if (formData.get('date')) {
+			obj = {
+				email: formData.get('email'),
+				url: formData.get('url'),
+				byDate: formData.get('date'),
+			};
+			url = 'https://doctor-sappointment.onrender.com/api/tasks/bySelectedDate';
+		} else {
+			obj = {
+				email: formData.get('email'),
+				url: formData.get('url'),
+			};
+			url = 'https://doctor-sappointment.onrender.com/api/tasks/nearestTicket';
+		}
 
-		// const obj = {
-		// 	email: formData.get('email'),
-		// 	url: formData.get('url'),
-		// };
-		// new Promise((res, rej) => {
-		// 	setTimeout(() => {
-		// 		loadingMessage.textContent = 'почти добежал...';
-		// 	}, 5000);
-		// 	fetch('https://doctor-sappointment.onrender.com/api/addTask', {
-		// 		method: 'POST',
-		// 		headers: { 'Content-Type': 'application/json' },
-		// 		body: JSON.stringify(obj),
-		// 	}).then((result) => res(result));
-		// })
-		// 	.then(() => {
-		// 		loading.style.display = 'none';
-		// 		success.style.display = 'flex';
-		// 		form.reset();
-		// 	})
-		// 	.catch((err) => {
-		// 		loading.style.display = 'none';
-		// 		error.style.display = 'flex';
-		// 		console.log(err);
-		// 	})
-		// 	.finally(() => {
-		// 		setTimeout(() => {
-		// 			error.style.display = 'none';
-		// 			success.style.display = 'none';
-		// 		}, 5000);
-		// 	});
+		new Promise((res, rej) => {
+			setTimeout(() => {
+				loadingMessage.textContent = 'почти добежал...';
+			}, 5000);
+			fetch(url, {
+				method: 'POST',
+				headers: { 'Content-Type': 'application/json' },
+				body: JSON.stringify(obj),
+			}).then((result) => res(result));
+		})
+			.then(() => {
+				loading.style.display = 'none';
+				success.style.display = 'flex';
+				form.reset();
+			})
+			.catch((err) => {
+				loading.style.display = 'none';
+				error.style.display = 'flex';
+				console.log(err);
+			})
+			.finally(() => {
+				setTimeout(() => {
+					error.style.display = 'none';
+					success.style.display = 'none';
+				}, 5000);
+			});
 	};
 
 	form.addEventListener('submit', submit);
