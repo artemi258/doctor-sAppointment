ea8b1cccb1bf3a2b5ecd5a8aa76a2b27fd5c4edc
Artemiy <artem2k17xia@yandex.ru>
2023-09-12 21:37:00 +0500

    update js files

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,13 @@
-import '../style/style.scss';
 import { clickOnSpace } from './modules/clickOnSpace';
 import { dateRestriction } from './modules/dateRestriction';
 import { submittingForm } from './modules/submittingForm';
 import { validate } from '../utils/validate';
 import { isCheckboxByDate } from './modules/isCheckboxByDate';
+import { yandexMetrika } from './modules/yandexMetrika';
+
+import '../style/style.scss';
+
+process.env.NODE_ENV === 'production' ? null : yandexMetrika();
 
 window.addEventListener('DOMContentLoaded', () => {
 	dateRestriction();
