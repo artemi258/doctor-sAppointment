ed87c45283fceadda7122c2556cd1fe6f0477aa0
Artemiy <artem2k17xia@yandex.ru>
2023-10-03 11:19:47 +0500

    update html

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,12 @@
 import { submittingForm } from './modules/submittingForm';
 import { validate } from '../utils/validate';
 import { isCheckboxByDate } from './modules/isCheckboxByDate';
-import { yandexMetrika } from './modules/yandexMetrika';
+// import { yandexMetrika } from './modules/yandexMetrika';
 
 import '../style/style.scss';
 import { calendar } from './modules/calendar';
 
-process.env.NODE_ENV === 'production' ? yandexMetrika() : null;
+// process.env.NODE_ENV === 'production' ? yandexMetrika() : null;
 
 window.addEventListener('DOMContentLoaded', () => {
   submittingForm();
